c7c726c678d29b4cb4bfa31fbe8dac1c08b6aa5b
CodieDigital <tiago.maciel@leanwork.com.br>
2022-07-06 18:37:09 -0300

    feat: improvements to deploy

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,17 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ['http://localhost:3030/', 'http://localhost:3030'],
-    methods: ['POST', 'PUT', 'DELETE', 'GET'],
-    credentials: true,
-  });
+  app.enableCors();
+  //   {
+  //   origin: [
+  //     'http://localhost:3030/',
+  //     'http://localhost:3030',
+  //     'https://codigo-fonte.vercel.app/login/',
+  //   ],
+  //   methods: ['POST', 'PUT', 'DELETE', 'GET'],
+  //   credentials: true,
+  // }
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
